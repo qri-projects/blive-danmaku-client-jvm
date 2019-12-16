09b47cd763fb45a20dd7c270da8b4e2c44c891f3
HHHHHG <2894700792@qq.com>
2019-12-16 20:25:03 +0800

    优化sample

--- src/test/java/com/ggemo/bilidanmakuclient/Sample.java
+++ src/test/java/com/ggemo/bilidanmakuclient/Sample.java
@@ -12,9 +12,9 @@ import org.junit.Test;
 
 
 public class Sample {
-    private static final long ROOM_ID = 4767523;
-
     public static void simplePrint(long roomId) {
+        // 需要使用一个HandlerHolder对象,在这个对象里定义响应弹幕的操作
+        // HandlerHolder由两部分组成,一部分是处理观众信息的UserCountHandler,一部分是处理其他事件的CmdHandler
         HandlerHolder handlerHolder = new HandlerHolder();
 
         // 收到房间观众数信息时打印房间观众数
@@ -24,6 +24,7 @@ public class Sample {
                 System.out.println(x);
             }
         });
+
         // 收到事件时打印事件(字符串)
         handlerHolder.addCmdHandler(new CmdHandler() {
             @Override
@@ -32,15 +33,19 @@ public class Sample {
             }
         });
 
+        // 创建BiliLiveDanmakuClient对象,使用房间号和HandlerHolder作为实例化参数
         BiliLiveDanmakuClient client = new BiliLiveDanmakuClient(roomId, handlerHolder);
+
+        // 开始监听(这个start是个同步操作)
         client.start();
     }
 
     public static void oopPrint(long roomId) {
-        OopCmdHandler cmdHandler = new OopCmdHandler();
+        // OopCmdHandler实现了CmdHandler接口, 本质是将事件字符串转换为对应对象
+        OopCmdHandler oopCmdHandler = new OopCmdHandler();
 
         // 收到弹幕时打印com.ggemo.bilidanmakuclient.oop.cmddata.DanmakuData对象
-        cmdHandler.addDanmakuHandler(new DanmakuHandler() {
+        oopCmdHandler.addDanmakuHandler(new DanmakuHandler() {
             @Override
             public void handle(DanmakuData x) {
                 System.out.println(x);
@@ -48,7 +53,7 @@ public class Sample {
         });
 
         // 收到礼物时打印com.ggemo.bilidanmakuclient.oop.cmddata.SendGiftData对象
-        cmdHandler.addSendGiftHandler(new SendGiftHandler() {
+        oopCmdHandler.addSendGiftHandler(new SendGiftHandler() {
             @Override
             public void handle(SendGiftData x) {
                 System.out.println(x);
@@ -56,7 +61,7 @@ public class Sample {
         });
 
         // 有观众上舰时打印com.ggemo.bilidanmakuclient.oop.cmddata.GuardBuyData对象
-        cmdHandler.addGuardBuyHandler(new GuardBuyHandler() {
+        oopCmdHandler.addGuardBuyHandler(new GuardBuyHandler() {
             @Override
             public void handle(GuardBuyData x) {
                 System.out.println(x);
@@ -64,7 +69,7 @@ public class Sample {
         });
 
         // 收到醒目留言时打印com.ggemo.bilidanmakuclient.oop.cmddata.SuperChatData对象
-        cmdHandler.addSuperchatHandler(new SuperchatHandler() {
+        oopCmdHandler.addSuperchatHandler(new SuperchatHandler() {
             @Override
             public void handle(SuperChatData x) {
                 System.out.println(x);
@@ -75,8 +80,11 @@ public class Sample {
 
         // 收到房间观众数信息时打印房间观众数
         handlerHolder.addUserCountHandler(x -> System.out.println(x));
-        handlerHolder.addCmdHandler(cmdHandler);
 
+        // 使用oopCmdHandler作为handlerHolder的cmdHandler
+        handlerHolder.addCmdHandler(oopCmdHandler);
+
+        // 创建BiliLiveDanmakuClient对象,使用房间号和HandlerHolder作为实例化参数
         BiliLiveDanmakuClient client = new BiliLiveDanmakuClient(roomId, handlerHolder);
         client.start();
     }
